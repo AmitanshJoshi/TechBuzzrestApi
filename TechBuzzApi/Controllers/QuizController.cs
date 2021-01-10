048f5031483d6ace41eb645d0be55c7bd78f4566
AmitanshJoshi <52008698+AmitanshJoshi@users.noreply.github.com>
2021-01-10 13:40:40 +0530

    Break the code for jenkins build

--- TechBuzzApi/Controllers/QuizController.cs
+++ TechBuzzApi/Controllers/QuizController.cs
@@ -15,6 +15,7 @@ namespace TechBuzzApi.Controllers
         [Route("api/Questions")]
         public HttpResponseMessage GetQuestions()
         {
+        unused variable
             using (Techbuzz_testEntities db = new Techbuzz_testEntities())
             {
                 var Qns = db.Questions
