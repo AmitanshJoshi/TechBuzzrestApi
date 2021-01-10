975a233b8b7c06066761dcdee78fdd0e1b39c3e2
AmitanshJoshi <52008698+AmitanshJoshi@users.noreply.github.com>
2021-01-10 18:39:46 +0530

    Corrected

--- TechBuzzApi/Controllers/QuizController.cs
+++ TechBuzzApi/Controllers/QuizController.cs
@@ -15,7 +15,7 @@ namespace TechBuzzApi.Controllers
         [Route("api/Questions")]
         public HttpResponseMessage GetQuestions()
         {
-        unused 
+        
             using (Techbuzz_testEntities db = new Techbuzz_testEntities())
             {
                 var Qns = db.Questions
